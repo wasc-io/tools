cb3e55c94d0550fd0ee26faa43ae7a97640b4f2b
Nicolai Schmid <nicolai@schmid.uno>
2018-01-23 14:35:49 +0100

    Feat: Added ecmaVersion to eslintrc for tool configuration

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['wasc', 'prettier'],
   plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 6,
+  },
   // Turn off rules that are too strict
   // TODO: Remove these rules when we updated eslint-config-blogfoster
   rules: {
