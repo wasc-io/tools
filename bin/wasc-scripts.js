d37ea584a141b776113a1dcd2b260b34ee3503e8
Nicolai Schmid <nicolai@schmid.uno>
2018-01-27 01:14:07 +0100

    Feat: Added a --help cli option

--- bin/wasc-scripts.js
+++ bin/wasc-scripts.js
@@ -1,4 +1,5 @@
 const script = process.argv.length > 1 ? process.argv[2] : undefined;
+const chalk = require('chalk');
 
 switch (script) {
     case 'lint':
@@ -12,6 +13,21 @@ switch (script) {
         const { version } = require('../package.json');
         console.log(version);
 
+        break;
+    }
+    case '--help': {
+        console.log(chalk.red.bold('wasc-tools'));
+        console.log(`A simple CLI for testing, linting, formatting Javascript projects, with the company required rules`);
+        console.log(chalk`
+    wasc-tools [script]
+
+    ${chalk.red('lint')}   Liting all supported files with Eslint
+    ${chalk.red('format')} Format all supported files with Prettier
+    ${chalk.red('test')}   Running tests in test/ directory with mocha
+    ${chalk.red('count')}  Count all lines and display stats about how many lines 
+            are commented, empty or contain TODOs
+    ${chalk.red('todo')}   Parse all source files for TODOs and create a TODOS.md file with all results
+`);
         break;
     }
     default: {
