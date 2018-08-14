4dcfaf0b2373da8709d75fe6dec8528cc11ef6fb
Nicolai Schmid <hey@schmid.uno>
2018-08-14 19:50:20 +0200

    feat(lint): switched to babel-eslint

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
+  parser: 'babel-eslint',
   extends: ['airbnb', 'prettier'],
   plugins: ['import', 'jsx-a11y', 'react', 'prettier'],
   parserOptions: {
+    sourceType: 'module',
     ecmaVersion: 2018,
   },
   // Turn off rules that are too strict
