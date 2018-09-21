edba011cbe523ad2275687b1d7d6ebc1a1300fe4
Nicolai Schmid <hey@schmid.uno>
2018-09-21 02:53:53 +0200

    feat: switched to using require.resolve for babel js transformer

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,12 +1,9 @@
-const babelJest = require('babel-jest');
-
-const babelrc = require('./.babelrc');
 const paths = require('./paths');
 
 module.exports = {
   rootDir: paths.projectRoot,
   cacheDirectory: '~/.cache',
   transform: {
-    '^.+\\.jsx?$': babelJest.createTransformer(babelrc),
+    '^.+\\.jsx?$': require.resolve('./jest/JSTransformer.js'),
   },
 };
---
+++ config/jest/JSTransformer.js
@@ -0,0 +1,4 @@
+const babelJest = require('babel-jest');
+const babelrc = require('../.babelrc');
+
+module.exports = babelJest.createTransformer(babelrc);
