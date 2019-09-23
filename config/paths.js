752819768b2c3381f001845d685b4cf1234b2fb5
Nicolai Schmid <nicolai@schmid.uno>
2019-09-23 18:51:34 +0200

    feat: switch prettier to stream based js-implementation

--- config/paths.js
+++ config/paths.js
@@ -18,12 +18,10 @@ module.exports = {
 
   selfIgnore: resolveSelfDirectory('config/.ignore'),
 
-  selfPrettier: resolveSelfDirectory('node_modules/.bin/prettier'),
   selfJest: resolveSelfDirectory('node_modules/.bin/jest'),
   selfBabelNode: resolveSelfDirectory('node_modules/.bin/babel-node'),
 
   selfESLintConfig: resolveSelfDirectory('config/.eslintrc.js'),
-  selfPrettierConfig: resolveSelfDirectory('config/.prettierrc.json'),
   selfJestConfig: resolveSelfDirectory('config/jest.config.js'),
   selfBabelConfig: resolveSelfDirectory('config/.babelrc.js'),
 };
