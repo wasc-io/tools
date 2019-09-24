5d5aba0813d63cc6adf2080c9e2ef63d6d9bff52
Nicolai Schmid <nicolai@schmid.uno>
2019-09-24 14:28:36 +0200

    fix(format): use correct stream for prettier formatting

--- scripts/format.js
+++ scripts/format.js
@@ -8,9 +8,9 @@ module.exports = argv => {
   const gulpStream = gulp.src(`${paths.projectSrc}/**/*.{js,json,graphql,md,html}`);
   let prettierStream;
   if (argv['dry-run']) {
-    prettierStream.pipe(prettier.check(prettierConfig));
+    prettierStream = gulpStream.pipe(prettier.check(prettierConfig));
   } else {
-    prettierStream.pipe(prettier(prettierConfig));
+    prettierStream = gulpStream.pipe(prettier(prettierConfig));
   }
 
   prettierStream.pipe(gulp.dest(paths.projectSrc));
