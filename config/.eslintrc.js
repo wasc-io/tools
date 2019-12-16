44dedff8d5ad71473da8e85b66f0c222e6919a42
Nicolai Schmid <nicolai@schmid.uno>
2019-12-16 21:22:28 +0100

    feat: add eslint json plugin

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier'],
-  plugins: ['import', 'jsx-a11y', 'react', 'prettier'],
+  plugins: ['import', 'jsx-a11y', 'react', 'prettier', 'json'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2018,
