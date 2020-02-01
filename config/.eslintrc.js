eaf73865ad013b8246427a590b62d1c9ccdb44e9
Nicolai Schmid <nicolai@schmid.uno>
2020-02-01 23:36:32 +0100

    wip

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier'],
-  plugins: ['import', 'jsx-a11y', 'react', 'prettier', 'json'],
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'react',
+    'prettier',
+    'json',
+    '@typescript-eslint',
+  ],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
