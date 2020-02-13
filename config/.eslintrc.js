3a4481b433ee32ccd13eb409a34dd1398e8d9fd0
Nicolai Schmid <nicolai@schmid.uno>
2020-02-14 00:11:34 +0100

    feat(lint): add optimize-regex eslint

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier', 'plugin:css-modules/recommended'],
-  plugins: ['import', 'jsx-a11y', 'react', 'prettier', 'json', 'css-modules'],
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'react',
+    'prettier',
+    'json',
+    'css-modules',
+    'optimize-regex',
+  ],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
