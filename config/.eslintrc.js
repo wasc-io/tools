64da2980257193bb3f661b0e223572b0b216917f
Nicolai Schmid <nicolai@schmid.uno>
2020-02-14 00:40:08 +0100

    feat(lint): add filenames eslint plugin; allow camelCase and pascal case

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -15,9 +15,16 @@ module.exports = {
     'css-modules',
     'optimize-regex',
     'no-secrets',
+    'filenames',
   ],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
+  },
+  rules: {
+    'filenames/match-regex': 2,
+    'filenames/match-regex': [2, '^[a-zA-Z]+$', true],
+    'filenames/match-exported': [2, 'pascal'],
+    // 'filenames/no-index': 2,
   },
 };
