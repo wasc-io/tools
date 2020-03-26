4c32f19c666a123832e145aba9173ee79c0725ef
Nicolai Schmid <nicolai@schmid.uno>
2020-03-26 21:42:55 +0100

    chore: remove local eslintrc

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-const config = require('./config/.eslintrc.js');
-
-module.exports = Object.assign(config, {
-  rules: Object.assign(config.rules, {
-    'filenames/match-regex': [2, '^[a-zA-Z]+(\\.config)?$', true],
-  }),
-});
