5520650dabad2b1ec3eb5fd37ff4857ac3960d7e
Nicolai Schmid <nicolai@schmid.uno>
2020-07-16 16:48:09 +0200

    fix: use correct options for nodeExternals

--- config/webpack.js
+++ config/webpack.js
@@ -34,7 +34,7 @@ module.exports = (argv) => {
     externals: [
       nodeExternals({
         // load non-javascript files with extensions, presumably via loaders
-        allowList: [/\.(?!(?:jsx?|json)$).{1,5}$/i, /@internal/],
+        allowlist: [/\.(?!(?:jsx?|json)$).{1,5}$/i, /@internal/],
         modulesFromFile: true,
       }),
     ],
