5f291ce1c3e37aa96e79ecbe98cfff9438b6cf7d
Nicolai Schmid <nicolai@schmid.uno>
2020-07-25 17:14:57 +0200

    feat(test): setup environment with dotenv for tests

--- config/jest.config.js
+++ config/jest.config.js
@@ -3,6 +3,7 @@ const paths = require('./paths');
 module.exports = {
   rootDir: paths.projectRoot,
   cacheDirectory: paths.projectCache,
+  setupFiles: ['dotenv/config'],
   transform: {
     '^.+\\.jsx?$': require.resolve('./jest/JSTransformer.js'),
   },
